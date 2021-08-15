c2104b3857b71515484f9601efacac9bdfe5a366
EvgeniyKostin <kostinen@icloud.com>
2021-08-16 00:30:00 +0300

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Evgeniy Kostin on 16.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
