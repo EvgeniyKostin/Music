c83bc8b1af7c40cc23e85f299a78ff97096f3353
EvgeniyKostin <kostinen@icloud.com>
2021-08-16 23:18:37 +0300

    cell settings

--- Music/SearchViewController.swift
+++ Music/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+//создаем модель
 struct TrackModel {
     var trackName: String
     var artistName: String
@@ -14,6 +15,9 @@ struct TrackModel {
 
 //данный экран будет отвечать за поиск треков из internet
 class SearchViewController: UITableViewController {
+    
+    let tracks = [TrackModel(trackName: "bad guy", artistName: "Billie Eilish"),
+                 TrackModel(trackName: "bury a friend", artistName: "Billie Eilish")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +27,18 @@ class SearchViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
     }
     
+    //создаем метод numberOfRowsInSection отвечающий за количество ячеек в таблице
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tracks.count
     }
     
+    //создаем метод cellForRowAt отвечающий за настройку конкретной ячейки
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "indexPath: \(indexPath)"
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "Image-1")
         return cell
     }
     
