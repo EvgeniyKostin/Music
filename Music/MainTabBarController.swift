e05ce542a6ec370e101fe935a1aa5e9afcf4b797
EvgeniyKostin <kostinen@icloud.com>
2021-08-16 19:58:58 +0300

    MainTabBarController has been created.

---
+++ Music/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  Music
+//
+//  Created by Evgeniy Kostin on 16.08.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
