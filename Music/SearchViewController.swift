d320f27eaa63a1a7d5c2e7844c72e45afe85af1f
EvgeniyKostin <kostinen@icloud.com>
2021-08-20 22:08:09 +0300

    Alamofire is tested and works

--- Music/SearchViewController.swift
+++ Music/SearchViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 //создаем модель
 struct TrackModel {
@@ -58,5 +59,18 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
+        
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData { dataResponse in
+            if let error = dataResponse.error {
+                print("Error received rewuestiong data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+        }
     }
 }
